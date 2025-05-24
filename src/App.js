cd1482563c6b9a9293e3f94441cce952bd3f2077
MadhuVasamsetti <2300030709@kluniversity.in>
2025-05-24 20:24:49 +0530

    Normalize line endings

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop"; // Scroll behavior on route change
+import './App.css';
 
 function App() {
   return (
